f1bc8a2f800f315515751968eb2494ecd2635895
Manpreet-Tiwana <manpreettiwana028@gmail.com>
2024-11-06 10:31:25 +0530

    Final commit

---
+++ 1st self practice.py
@@ -0,0 +1,5 @@
+a= int(input("Enter your age to check whether you are eligible for voting or not:"))
+if(a>=18):
+    print("you can vote. Show me your voter card")
+else:
+    print("you are not able to vote.")
